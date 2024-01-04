95babaf7568277a3476274329f2957ce647ba72c
Fausto Sacufundala <155498985+fausto18@users.noreply.github.com>
2024-01-04 10:14:54 +0100

    Update ft_strcmp.c

--- Exam C01/ft_strcmp.c
+++ Exam C01/ft_strcmp.c
@@ -1,7 +1,9 @@
-int ft_strcmp(char *s1, char *s2)
+int		ft_strcmp(char *s1, char *s2)
 {
-	while (*s1++ == *s2++)
-		if (!*s1 && !*s2)
-			return (0);
-	return (*--s1 - *--s2);
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
 }
